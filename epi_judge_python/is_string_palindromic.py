97c26a0335951983aecd81375cf9007db479ebec
pavdemesh <pavdemesh@gmail.com>
2021-04-28 16:11:21 +0300

    solved

--- epi_judge_python/is_string_palindromic.py
+++ epi_judge_python/is_string_palindromic.py
@@ -2,7 +2,12 @@ from test_framework import generic_test
 
 
 def is_palindromic(s: str) -> bool:
-    # TODO - you fill in here.
+    if len(s) <= 1:
+        return True
+    half_indices = len(s) // 2
+    for i in range(half_indices):
+        if s[i] != s[-1 - i]:
+            return False
     return True
 
 
--- problem_mapping.js
+++ problem_mapping.js
@@ -65,7 +65,7 @@ problem_mapping = {
                 "passed": 0,
                 "total": 10000
             },
-            "Python: closest_int_same_weight.py": { 
+            "Python: closest_int_same_weight.py": {
                 "passed": 0,
                 "total": 10000
             }
@@ -476,7 +476,7 @@ problem_mapping = {
                 "total": 10000
             },
             "Python: is_string_palindromic.py": {
-                "passed": 0,
+                "passed": 10000,
                 "total": 10000
             }
         },
